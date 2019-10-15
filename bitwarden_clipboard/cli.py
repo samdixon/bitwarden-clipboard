53fce0ef4c25eec863a3d0a7126348893eefbc29
sjdsh <samljdixon@gmail.com>
2019-10-15 10:18:45 -0400

    Adds Argument Parser function Adds extremely basic binary checking Adds logging

---
+++ bitwarden_clipboard/cli.py
@@ -0,0 +1,44 @@
+import argparse
+import logging
+import shutil
+
+logger = logging.getLogger(__name__)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="This app")
+    parser.add_argument("command", default="fzf", nargs="?")
+    parser.add_argument("--update-cache", action="store_true")
+    parser.add_argument("--rebase-cache", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    return args
+
+def set_log_level(debug: bool) -> None:
+    if debug:
+        logging.basicConfig(filename="debug.log", level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+def check_binaries(binaries: list) -> dict:
+    values = {}
+    for binary in binaries:
+        if shutil.which(binary):
+            values[binary] = shutil.which(binary)
+        else:
+            values[binary] = False
+
+    return values
+
+def confirm_binary_values(values: dict) -> bool:
+    for key, val in values.items():
+        if values[key] == False:
+            print(f"Binary {key} not found. Please install before proceeding.")
+            exit()
+        else:
+            return True
+
+def cli():
+    args = get_args()
+    set_log_level(args.debug)
+    confirm_binary_values(check_binaries(["fzf", "bw"]))
